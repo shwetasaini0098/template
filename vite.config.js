efdcd899ce27f929aba773fc73197f81fced35da
Shweta Saini <shwetasaini0098@gmail.com>
2025-06-29 21:59:18 +0530

    Final cleanup and ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-<<<<<<< HEAD
 export default defineConfig({
   base: '/template/',
-=======
-// https://vite.dev/config/
-export default defineConfig({
->>>>>>> 624e8ae806de7c78277fc812df51cea5ef40fa68
   plugins: [react()],
-})
+});
